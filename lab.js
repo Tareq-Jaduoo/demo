65d7a5bb058cad5e88f142b62753fae8170c77b9
Tareq-Jaduoo <ta.jaduoo@gmail.com>
2021-01-28 21:22:07 +0200

    90

--- lab.js
+++ lab.js
@@ -1,9 +1,34 @@
 
-var Planttype=prompt (' enter the Plant type?');
-var Plantnumber=prompt ('enter Plant number ?  ');
-var Discount;
+var Planttype = prompt('what do you want Flowers or Cactus');
 
+while (Planttype !== 'Flowers' && Planttype !== 'Cactus') {
+ Planttype = prompt('what do you want Flowers or Cactus');
+}
+
+var plant = '';
+
+if (Planttype === 'Cactus') {
+    plant = '<img src="Cactus.jpg"/>';
+} else if (Planttype === 'Flowers ') {
+    plant = '<img src="Flowers.jpg"/>';
+    }
+
+    var yasPlant = prompt('enter Plant number ???  ');
+
+var plants ='';
+for (var i = 0; i< yasPlant; i++) {
+    plant =plant +yasPlant;
+}
+
+    document.write(Planttype)
+
+      
 alert('Lets make life prosperous')
+var Plantnumber= prompt('enter Plant number ?  ');
+
+var Plantnumber ;
+for (Plantnumber = 0; Planttype > 0; Plantnumber++) {
+}
 if (Plantnumber > 18) {
     Discount = '25%'; 
 }
@@ -19,3 +44,4 @@ else {
     Discount = 'Welcome';
 }
 document.write("Enjoy your"+Discount ) 
+s
