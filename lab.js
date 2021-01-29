c6b6fb833ddcc4caafc59401689f7fda95c279b6
Tareq-Jaduoo <ta.jaduoo@gmail.com>
2021-01-29 17:56:36 +0200

    dane

--- lab.js
+++ lab.js
@@ -23,25 +23,25 @@ for (var i = 0; i< yasPlant; i++) {
     document.write(Planttype)
 
       
-alert('Lets make life prosperous')
-var Plantnumber= prompt('enter Plant number ?  ');
-
-var Plantnumber ;
-for (Plantnumber = 0; Planttype > 0; Plantnumber++) {
-}
-if (Plantnumber > 18) {
-    Discount = '25%'; 
-}
-else if (Plantnumber > 12) {
-    Discount = '15%';
-}
-
-else if (Plantnumber<12) {
-    Discount= ' 5%';
-}
-
-else {
-    Discount = 'Welcome';
-}
-document.write("Enjoy your"+Discount ) 
-s
+    alert('Lets make life prosperous')
+    var Plantnumber= prompt('enter Plant number ?  ');
+    
+    var Plantnumber ;
+    for (Plantnumber = 0; Planttype > 0; Plantnumber++) {
+    }
+    if (Plantnumber > 18) {
+        Discount = '25%'; 
+    }
+    else if (Plantnumber > 12) {
+        Discount = '15%';
+    }
+    
+    else if (Plantnumber<12) {
+        Discount= ' 5%';
+    }
+    
+    else {
+        Discount = 'Welcome';
+    }
+    document.write("Enjoy your"+Discount ) 
+    s
